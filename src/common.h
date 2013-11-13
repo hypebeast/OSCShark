223c3412948bad61d03ad2ec3684912ebe6adc6b
Sebastian Ruml <sebastian.ruml@gmail.com>
2013-11-13 10:48:18 +0100

    updated LICENSE header

--- src/common.h
+++ src/common.h
@@ -1,7 +1,7 @@
 /*
  * The MIT License (MIT)
  *
- * Copyright (c) <2013> <Sebastian Ruml>
+ * Copyright (c) 2013 Sebastian Ruml
  *
  * Permission is hereby granted, free of charge, to any person obtaining a
  * copy of this software and associated documentation files (the "Software"),
